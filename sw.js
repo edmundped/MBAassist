e3567af4cfe3cfad0be26b36ec8948c67517a301
Edmund Dotsey <edotsey@alxafrica.com>
2026-07-24 20:49:04 +0000

    Add session-based learning coach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mba512-learning-space-v3";
+const CACHE = "mba512-learning-space-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
