075f13d9cbc26300fdb2a871ecac168d5058f9e1
Edmund Dotsey <edotsey@alxafrica.com>
2026-07-24 20:37:40 +0000

    Modernize learning space UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mba512-learning-space-v2";
+const CACHE = "mba512-learning-space-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
