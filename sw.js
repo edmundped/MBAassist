70cf3e173576b30e9add7ef3689343e4fb7e70b1
Edmund Dotsey <edotsey@alxafrica.com>
2026-08-06 18:02:33 +0000

    Audit drill answer calculations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mbaassist-v5";
+const CACHE = "mbaassist-v6";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/study-hero.png"];
 
 self.addEventListener("install", e => {
