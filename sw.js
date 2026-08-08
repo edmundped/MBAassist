a4ad7094f1e484ea33b84c2b214df77ce77d44b4
Edmund Dotsey <edotsey@alxafrica.com>
2026-08-08 13:56:53 +0000

    Add slide examples to exam prep

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mbaassist-v13";
+const CACHE = "mbaassist-v14";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/study-hero.png"];
 
 self.addEventListener("install", e => {
