b1dd8cbff3aff1a03d6431b5bdcf77e87207fabd
Edmund Dotsey <edotsey@alxafrica.com>
2026-08-08 13:33:33 +0000

    Add quiz back navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mbaassist-v11";
+const CACHE = "mbaassist-v12";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/study-hero.png"];
 
 self.addEventListener("install", e => {
