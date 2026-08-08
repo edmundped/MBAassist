16684234fc8838be3be67acc06b37adc09d00806
Edmund Dotsey <edotsey@alxafrica.com>
2026-08-08 13:46:23 +0000

    Simplify exam prep menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mbaassist-v12";
+const CACHE = "mbaassist-v13";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/study-hero.png"];
 
 self.addEventListener("install", e => {
